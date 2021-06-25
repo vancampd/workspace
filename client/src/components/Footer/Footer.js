3847ea2d26929a0f44d27a858c2b6d6f4ca88227
Daniel Van Camp <vancampdj@gmail.com>
2021-06-25 10:19:09 -0400

    Added .env_sample files. Removed any responsive styling in order to display my mobile styles on full screen view with my project centered, recreating a mobile-type design.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -11,10 +11,16 @@ class Footer extends Component {
                     <h3 className='footer__header'>About us</h3>
                     <p className='footer__text'>
                         We know businesses have been greatly affected over the past year. 
-                        One of those is more employees working remotely. As a result, many businesses have space that isn't being used effectively. 
+                        One change is more employees working remotely. As a result, many businesses have space that isn't being used effectively. 
                         We want to help increase the occupancy rate of your space, and highlight the 'OR' in our name because of that.
-                        If you have space not being used, post it with us and let us help find you the right partner to rent the space, for any amount of time!
+                        If you have space not being used, post it with us and let us help find you the right partner to rent your space.
                     </p>
+                </div>
+                <div className='footer__info-container'>
+                    <h3 className='footer__header'>Developed by Danny Van Camp</h3>
+                    <a className='footer__text' href='http://github.com/vancampd'>
+                        http://github.com/vancampd
+                    </a>
                 </div>
             </footer>           
         )
